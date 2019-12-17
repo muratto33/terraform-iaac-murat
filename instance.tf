e51bf0e79de4ab63648ac65f3d5b37d995e26433
muratto33 <muratkilic33@yahoo.com>
2019-12-16 20:27:10 -0600

    added key

--- instance.tf
+++ instance.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web" {
   ami           = "AMI_ID_HERE" 
   instance_type = "t2.micro" 
  
- associate_public_ip_address = "true" 
+ associate_public_ip_address = "true"
+ key_name = "${aws_key_pair.deployer.key_name}" 
  tags = { 
     Name = "HelloWorld" 
   } 

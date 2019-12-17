e10228426670508bdc05ed0b2528db287d490ac3
muratto33 <muratkilic33@yahoo.com>
2019-12-16 20:27:57 -0600

    add

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro" 
  
  associate_public_ip_address = "true"
- key_name = "${aws_key_pair.deployer.key_name}" 
+ key_name = "aws_key_pair.deployer.key_name" 
  tags = { 
     Name = "HelloWorld" 
   } 

822196e58e5bba6f9509dd8eb0e51bfb86d64ac1
muratto33 <muratkilic33@yahoo.com>
2019-12-16 20:49:24 -0600

    add

--- instance.tf
+++ instance.tf
@@ -1,12 +1,18 @@
-resource "aws_instance" "web" { 
-  ami           = "ami-00068cd7555f543d5" 
-  instance_type = "t2.micro" 
- 
- associate_public_ip_address = "true"
- key_name = aws_key_pair.deployer.key_name
+resource "aws_instance" "web" {
+  ami             = "ami-00068cd7555f543d5"
+  instance_type   = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name = aws_key_pair.deployer.key_name
 
- user_data = file("userdata_file)
- tags = { 
-    Name = "HelloWorld" 
-  } 
-} 
+
+
+  user_data = file("userdata_file")
+
+
+
+  
+
+tags = {
+    Name = "HelloWorld"
+  }
+}

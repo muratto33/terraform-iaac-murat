0ba9bc760685f3d7cdc1988eb33ca3a132f4ec49
muratto33 <muratkilic33@yahoo.com>
2020-01-04 10:23:52 -0600

    add

--- iam.tf
+++ iam.tf
@@ -8,7 +8,7 @@ resource "aws_iam_group" "developers" {
     path = "/" 
 } 
 
-resource "aws_iam_group_membership" "team" { 
+resource "aws_iam_group_membership" "developers_team" { 
   name = "developers-group-membership" 
   users = [ 
     aws_iam_user.tim.name 

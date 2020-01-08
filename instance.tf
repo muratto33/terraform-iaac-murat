26a7d373b9ef0357b30e4ced559a8d3548555169
muratto33 <muratkilic33@yahoo.com>
2020-01-07 18:48:08 -0600

    add

--- instance.tf
+++ instance.tf
@@ -4,18 +4,25 @@ resource "aws_instance" "web" {
   instance_type   = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
-  security_groups = ["allow_ssh1"]
+  security_groups = ["allow_ssh"]
   provisioner "remote-exec" {
     connection {
       host = self.public_ip
       type = "ssh"
       user = var.user
       private_key = file(var.ssh_key_location)
-    }
-      
-  
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd mc -y ",
+        "sudo systemctl start httpd",
+        ]
+      }
 
-   tags = {
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
     Name = "HelloWorld${count.index +1}"
-   }
+  }
 }

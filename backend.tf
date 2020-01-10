8ecca7ac770a166e523b690c1cd9da74e3313361
muratto33 <muratkilic33@yahoo.com>
2020-01-10 06:32:00 -0600

    delete

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bakend-state-muratt"
-    key    = "infrastructure"
-    region = "us-east-1"
-  }
-}
--- route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "www" { 
-  zone_id = "Z3Q6Y7MR318A1F" 
-  name    = "www.silkysword.com" 
-  type    = "A" 
-  ttl     = "60" 
-  records = [aws_instance.web[0].public_ip]
-} 

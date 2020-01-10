41930b61c36e5cbcabae82ac675efb7507511829
muratto33 <muratkilic33@yahoo.com>
2020-01-10 05:57:04 -0600

    add

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-c83b27ae"
+  vpc_id      = "vpc-79226b03"
 
 
   ingress {
